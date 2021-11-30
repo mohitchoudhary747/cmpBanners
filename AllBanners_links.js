c006b7e99677cb7b2fcbfc43208f1d2a74644ebd
Mohit Choudhary <mohit.choudhary@securiti.ai>
2021-11-30 17:04:50 +0530

    ADDED DELL WEBSITES

---
+++ AllBanners_links.js
@@ -0,0 +1,47 @@
+
+//Base URl
+//          https://mohitchoudhary747.github.io/cmpBanners/
+
+https://akk4grcycagblrx6brj6qw-on.drv.tw/cmp_testing_Website/QA_Websites/QA_PrimaryTenantWebSites/editOptOutMainTenant_2.html
+https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/QA_PrimaryTenantWebSites/editOptOutMainTenant_2.html
+
+// lite
+
+https://akk4grcycagblrx6brj6qw-on.drv.tw/cmp_testing_Website/QA_Websites/QA_LiteTenantWebsites/LiteTenantEditOpt2/editOptOutLite_blog_2.html
+
+// /Users/mohitchoudhary/Downloads/cmp_testing_Website/QA_Websites/QA_LiteTenantWebsites/LiteTenantEditOpt2/editOptOutLite_blog_2.html
+// https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/QA_LiteTenantWebsites/LiteTenantEditOpt2/editOptOutLite_blog_2.html
+
+
+
+
+// CMP_CookieBanner_CaptureCookieConsent_Revoke_Button
+// Litte
+// https://akk4grcycagblrx6brj6qw-on.drv.tw/cmp_testing_Website/QA_Websites/QA_LiteTenantWebsites/CaptureConsent/templatemo_541_host_cloud/QA_CpatureConsent_Lite_001.html
+// https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/QA_LiteTenantWebsites/CaptureConsent/templatemo_541_host_cloud/QA_CpatureConsent_Lite_001.html
+
+
+// CMP_Edit_Cookies_Categories_Phase1 Lite
+// https://dfaiaf7y1njownnmi5cz9w-on.drv.tw/mywebforms/ContactForm/LiteTenant007.html
+
+
+// Phase two edit category lite tenant
+// /Users/mohitchoudhary/Downloads/cmp_testing_Website/QA_Websites/QA_LiteTenantWebsites/EditCategoryPahse2/LiteTenant007.html
+// https://mohitchoudhary747.github.io/cmpBanners//QA_Websites/QA_LiteTenantWebsites/EditCategoryPahse2/LiteTenant007.html
+
+
+
+// primaru edit 1
+// https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/QA_PrimaryTenantWebSites/CookieCategory_Satge_1/CategoryPahse1_1.html
+// https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/GeoLocation_Banner/blog.html
+
+
+// {{@customizeContent_FormUrl}}
+// https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/Pref_Centre_CustomizeContent/Testing.html
+
+https://mohitchoudhary747.github.io/cmpBanners/Dev-Stage_Websites/Edit_OptOut/Dev-Stage_EditOptOut_5.html
+
+// Dell WEBSITES
+https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/QA_PrimaryTenantWebSites/Dell/www.delltechnologies.com/en-ca/delltechnologies_2.html
+https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/QA_PrimaryTenantWebSites/Dell/www.dell.com/dell_com_1.html
+https://mohitchoudhary747.github.io/cmpBanners/QA_Websites/QA_PrimaryTenantWebSites/Dell/www_dell_eu/content/dell_eu_1.html
