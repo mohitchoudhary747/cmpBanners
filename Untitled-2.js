929e7470e132542b9569da92d1de8b06e868c3a0
Mohit Choudhary <mohit.choudhary@securiti.ai>
2022-03-30 18:47:40 +0530

    Added Lates Code for Phase1

---
+++ Untitled-2.js
@@ -0,0 +1,5 @@
+
+
+//tr[descendant::*[text()='shafiq1646106059767@securiti.ai'] and descendant::*[text()='FormConsent']  and descendant::*[text()='ConsentPurposeNew'] and descendant::*[text()='News email'] and descendant::*[text()='GRANTED']]
+//tr[descendant::*[text()='{{@Email}}'] and descendant::*[text()='FormConsent']  and descendant::*[text()='ConsentPurposeNew'] and descendant::*[text()='News email'] and descendant::*[text()='GRANTED']]
+//tr[descendant::*[text()='{{@Email}}'] and descendant::*[text()='Form']]
